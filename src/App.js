8162833c68d117a9e3a2951cd4731e68cb600a84
Slv97 <Slv97@mail.ru>
2022-02-07 00:16:54 +0300

    Adding scrolling loading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [photos, setPhotos] = useState([]);
+    const [currenyPage, setCurrentPage] = useState(1); //1-500 pages
+    const [fetching, setFetching] = useState(true);
+    const [totalCount, setTotalCount] = useState(0);
+
+    useEffect(() => {
+        if (fetching) {
+            console.log('fetching');
+            axios
+                .get(
+                    `https://jsonplaceholder.typicode.com/photos?_limit=10&_page=${currenyPage}`
+                )
+                .then((response) => {
+                    setPhotos([...photos, ...response.data]);
+                    setCurrentPage((prevState) => prevState + 1);
+                    setTotalCount(response.headers['x-total-count'])
+                })
+                .finally(() => setFetching(false));
+        }
+    }, [fetching]);
+
+    useEffect(() => {
+        document.addEventListener("scroll", handleScroll);
+
+        return () => {
+            document.removeEventListener("scroll", handleScroll);
+        }
+    }, [totalCount]);
+
+    const handleScroll = (e) => {
+        let pageScrollheight = e.target.documentElement.scrollHeight;
+        let visibleArea = e.target.documentElement.scrollTop;
+        let windowHeight = window.innerHeight;
+        console.log(photos.length)
+        if (pageScrollheight - (visibleArea + windowHeight) < 100 && photos.length < totalCount) {
+            console.log("scroll");
+            setFetching(true);
+        }
+        //console.log("scrollHeight", e.target.documentElement.scrollHeight);
+        //console.log("scrollTop", e.target.documentElement.scrollTop);
+        //console.log("innerHeight", window.innerHeight);
+    };
+    return (
+        <div className="app">
+            {photos.map((photo) => (
+                <div className="photo" key={photo.id}>
+                    <div className="title">
+                        {photo.id}. {photo.title}
+                    </div>
+                    <img src={photo.thumbnailUrl} alt={photo.title} />
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default App;
